c44df9deff9ccec87e836f37f9d193a0d4399d57
Rene Felix Correa <renefc3@gmail.com>
2014-04-21 14:45:49 -0300

    Start

---
+++ src/Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeuEstacionamento.Dominio;
+
+namespace Tests
+{
+    [TestClass]
+    public class EstacionamentoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Estacionamento estacionamento = new Estacionamento();
+
+        }
+    }
+}
---
+++ src/dominio/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuEstacionamento.Dominio
+{
+    public abstract class BaseNegocio
+    {
+        public int Id { get; set; }
+    }
+
+    public class Veiculo : BaseNegocio
+    {
+        public string Placa { get; set; }
+        public string Fabricante { get; set; }
+        public string Modelo { get; set; }
+
+
+    }
+
+
+    public class Estacionamento : BaseNegocio
+    {
+        public int QtdVaga { get; set; }
+
+
+        public Vaga Estacionar(Veiculo veiculo)
+        {
+
+        }
+
+
+        public void Retirar(Veiculo veiculo)
+        {
+
+        }
+
+
+        public void Retirar(Vaga vaga)
+        {
+
+        }
+
+    }
+
+}
